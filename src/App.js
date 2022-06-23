6e24fc99108b8f912c91701ab52bcd129f46706c
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2022-06-24 02:43:51 +0530

    Delete handler Added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Create from './components/Create';
-import Note from './components/Note';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Create from "./components/Create";
+import Note from "./components/Note";
+import { useState } from "react";
 
 function App() {
-  const[a,b]=useState([]);
-  const addNote=(n)=>{
-  b((prevdata)=>{
-    return [...prevdata,n];
-  }
-  )
+  const [a, b] = useState([]);
+  const addNote = (n) => {
+    b((prevdata) => {
+      return [...prevdata, n];
+    });
+  };
+
+  const deleteNote = (index) => {
+    // Delete the note from the array using the index
+    b((prevdata) => {
+      return prevdata.filter((_, i) => {
+        return i !== index;
+      });
+    });
   };
   return (
     <div className="App">
-      <Header/>
-       <Create
-         passnote={addNote}
-       />
-      {a.map((val,index)=>{
-         return ( <Note
-           key={index}
-           id={index}
-           title={val.title}
-           content={val.content}
-         />
-         );
-       })}
-       
+      <Header />
+      <Create passnote={addNote} />
+      {a.map((val, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={val.title}
+            content={val.content}
+            deleteNote={deleteNote}
+            index={index}
+          />
+        );
+      })}
     </div>
   );
 }
